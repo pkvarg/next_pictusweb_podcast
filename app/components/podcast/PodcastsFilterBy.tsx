d89799c6b90ef85830b8f826b63146e282173172
peter varga <pkvarg@yahoo.se>
2024-08-31 21:35:51 +0200

    yet yet another add

--- app/components/podcast/PodcastsFilterBy.tsx
+++ app/components/podcast/PodcastsFilterBy.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const PodcastsFilterBy = () => {
-  console.log('added yet clg in pod filter by')
+  console.log('yet added yet clg in pod filter by')
   return (
     <div className='flex flex-row'>
       <p>Filtrovat by</p>
