f1b17cf27b7df90dcdac06ffa0eb5818b98ba797
peter varga <pkvarg@yahoo.se>
2024-09-01 08:41:12 +0200

    5envs

--- app/components/podcast/PodcastsFilterBy.tsx
+++ app/components/podcast/PodcastsFilterBy.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const PodcastsFilterBy = () => {
-  console.log('4env added yet clg in pod filter by')
+  console.log('5env added yet clg in pod filter by')
   return (
     <div className='flex flex-row'>
       <p>Filtrovat by</p>
