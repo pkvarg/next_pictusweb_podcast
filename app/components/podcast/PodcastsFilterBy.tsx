23da3bcb2f12c7f97bd8777b8ba507bb27ac3d39
peter varga <pkvarg@yahoo.se>
2024-09-01 07:37:08 +0200

    envs

--- app/components/podcast/PodcastsFilterBy.tsx
+++ app/components/podcast/PodcastsFilterBy.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const PodcastsFilterBy = () => {
-  console.log('yet added yet clg in pod filter by')
+  console.log('env added yet clg in pod filter by')
   return (
     <div className='flex flex-row'>
       <p>Filtrovat by</p>
