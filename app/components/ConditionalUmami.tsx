007067b3b63bba1ae74be1d38be6ccfb73ff55a5
peter varga <peter.varga@staffino.com>
2026-07-09 13:55:50 +0200

    new umami

--- app/components/ConditionalUmami.tsx
+++ app/components/ConditionalUmami.tsx
@@ -36,7 +36,7 @@ const ConditionalUmami = () => {
   return (
     <Script
       defer
-      src="https://umami-p00gs00gwcwo00s4k4c4kgg8.pictusweb.com/script.js"
+      src="https://analytics.pictusweb.com/script.js"
       data-website-id="a388ecb6-5425-4f32-afa7-62d945f69671"
     />
   )
